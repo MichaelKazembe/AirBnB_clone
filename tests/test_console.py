b6e342b450f53f3c4b5468d5479b8b20827c3fc6
ahmadoa <ouladaouida@gmail.com>
2023-07-16 11:28:38 -0700

    added test_cosnole

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """Tests for Console"""
-import unittest
-from unittest.mock import patch
-import os
-import console
-import tests
+from unittest import TestCase
 from console import HBNBCommand
-from io import StringIO
+import pycodestyle
 
 
-class TestConsole(unittest.TestCase):
+class TestHBNBCommand(TestCase):
     """tests for the console, quit and empty"""
 
-    @classmethod
-    def setUpClass(cls):
-        """setup console class for test"""
+    def test_pep(self):
+        """test pep"""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['console.py',
+                                    'tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    @classmethod
-    def teardown(cls):
-        """tears down console class"""
-  
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__('console').__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_class_doc(self):
+        """test class documentation"""
+        doc = HBNBCommand.__doc__
+        self.assertGreater(len(doc), 1)
