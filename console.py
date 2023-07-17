953b950bad0d2a5a09a70fcee5af8adf1a8e4458
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-07-17 12:54:12 +0200

    Update console.py

--- console.py
+++ console.py
@@ -129,18 +129,6 @@ class HBNBCommand(cmd.Cmd):
                              if type(obj).__name__ == argv[0]]
                 print(len(instances))
 
-    def dict_strip(self, st):
-        """tries to find a dict while stripping"""
-        newstring = st[st.find("(") + 1: st.rfind(")")]
-
-        try:
-            newdict = newstring[newstring.find("{") + 1: newstring.rfind("}")]
-            stripped_dict = eval("{" + newdict + "}")
-            return stripped_dict
-
-        except ValueError:
-            return None
-
     def default(self, arg):
         """Accepts class name followed by an argument"""
         argv = arg.split('.')
